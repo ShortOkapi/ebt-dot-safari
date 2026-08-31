4b0a020df5091b6e1d0bc18c37cb508bfa41ceb8
Miguel Viterbo <33769782+ShortOkapi@users.noreply.github.com>
2026-08-31 15:00:36 +0100

    Initial public release (v1.9.2)

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'ebt-dot-safari-v1.9.2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/site.webmanifest',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png',
+  '/favicon.ico',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('unpkg.com') || event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+        .catch(() => caches.match('/index.html'))
+    );
+  }
+});
