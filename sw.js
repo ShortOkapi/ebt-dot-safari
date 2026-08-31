f2c68c3d334b426fee2901b79306ee9321ee5776
Miguel Viterbo <33769782+ShortOkapi@users.noreply.github.com>
2026-08-31 17:51:38 +0100

    Update sw.js
    relative paths for assets being cached

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'ebt-dot-safari-v1.9.2';
+const CACHE_NAME = 'ebt-dot-safari-v1.9.3';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/site.webmanifest',
-  '/android-chrome-192x192.png',
-  '/android-chrome-512x512.png',
-  '/apple-touch-icon.png',
-  '/favicon-32x32.png',
-  '/favicon-16x16.png',
-  '/favicon.ico',
+  './',
+  './index.html',
+  './site.webmanifest',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './apple-touch-icon.png',
+  './favicon-32x32.png',
+  './favicon-16x16.png',
+  './favicon.ico',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
