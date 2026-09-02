8148d25a0ce70d2c98a97f5b06cfe24de082ea1b
Miguel Viterbo <33769782+ShortOkapi@users.noreply.github.com>
2026-09-02 22:16:55 +0100

    Set final version to 2.0.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.0.0-rc.1';
+const APP_VERSION = '2.0.0';
 const CACHE_PREFIX = 'ebt-dot-safari-v';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const ASSETS_TO_CACHE = [
