732bf8dfdf0664fe64b717ebc37db8eec0066c58
Miguel Viterbo <33769782+ShortOkapi@users.noreply.github.com>
2026-09-03 20:46:19 +0100

    Add memory-safe streaming CSV importer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.0.1';
+const APP_VERSION = '2.0.2-rc.1';
 const CACHE_PREFIX = 'ebt-dot-safari-v';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const ASSETS_TO_CACHE = [
